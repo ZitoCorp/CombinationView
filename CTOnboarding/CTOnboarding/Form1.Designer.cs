59c955daa13a4bfad17a2b073043134d02c96212
DemonZito <melanisticzito@gmail.com>
2020-05-04 22:05:55 +1200

    Uploaded local project

---
+++ CTOnboarding/CTOnboarding/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace CTOnboarding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 33);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 470);
+            this.splitContainer1.SplitterDistance = 425;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.LabelEdit = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(425, 470);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.Tag = "[Serializable]";
+            this.treeView1.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.treeView1_AfterLabelEdit);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.treeView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyDown);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(371, 470);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_CellMouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(165, 34);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(164, 30);
+            this.newToolStripMenuItem.Text = "New Child";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pasteToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(126, 34);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+    }
+}
+
---
+++ CTOnboarding/CTOnboarding/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CTOnboarding
+{
+    public partial class Form1 : Form
+    {
+        private TreeNode selectedNode;
+        //private TreeNode oldSelectedNode;
+        DataTable parsedData = new DataTable();
+
+        protected void treeView1_AfterSelect(object sender,
+System.Windows.Forms.TreeViewEventArgs e)
+        {
+            System.Console.WriteLine(treeView1.SelectedNode.Text);
+
+            if (treeView1.SelectedNode != null && dataGridView1.Columns[treeView1.SelectedNode.Text] != null)
+            {
+                if(selectedNode!=null && dataGridView1.Columns[selectedNode.Text] != null)
+                    dataGridView1.Columns[selectedNode.Text].Visible = false;
+
+                dataGridView1.Columns[treeView1.SelectedNode.Text].Visible = true;
+            }
+            //oldSelectedNode = selectedNode;
+
+            if(dataGridView1.Columns[treeView1.SelectedNode.Text] != null)
+                selectedNode = treeView1.SelectedNode;
+        }
+
+        protected void treeView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Delete) return;
+
+            TreeNode nodeToDelete = treeView1.SelectedNode;
+            TreeNode nextNode = treeView1.SelectedNode.PrevNode != null ? treeView1.SelectedNode.PrevNode : treeView1.SelectedNode.Parent;
+
+            treeView1.SelectedNode = nextNode;
+
+            if(parsedData.Columns[nodeToDelete.Text] != null)
+            {
+                //selectedNode = treeView1.SelectedNode.Parent;
+               // dataGridView1.Columns[treeView1.SelectedNode.Text].Visible = false;
+                parsedData.Columns.Remove(nodeToDelete.Text);
+            }
+            treeView1.Nodes.Remove(nodeToDelete);
+
+            //selectedNode = null;
+        }
+        private void DataGridView1_CellMouseClick(Object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right) return;
+
+            contextMenuStrip2.Show(dataGridView1, e.X, e.Y);
+        }
+            protected void treeView1_NodeMouseClick(object sender,
+    TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode curNode = treeView1.GetNodeAt(e.X,e.Y);
+
+            if (e.Button == MouseButtons.Left && curNode == selectedNode)
+            {
+                curNode.BeginEdit();
+            }
+
+            treeView1.SelectedNode = curNode;
+
+            
+
+
+            //selectedNode = curNode;
+
+            if (e.Button != MouseButtons.Right) return;
+
+
+            this.contextMenuStrip1.Show(treeView1, e.X,e.Y);
+        }
+
+    //    protected void treeView1_NodeMouseDoubleClick(object sender,
+    //TreeNodeMouseClickEventArgs e)
+    //    {
+    //        if (e.Button != MouseButtons.Right) return;
+
+    //        TreeNode curNode = treeView1.GetNodeAt(e.X, e.Y);
+    //        treeView1.SelectedNode = curNode;
+
+    //        curNode.BeginEdit();
+    //    }
+
+        private void SaveNodes(TreeNodeCollection nodesCollection, XmlWriter textWriter)
+        {
+            //foreach (var node in nodesCollection.OfType<TreeNode>().Where(x => x.Nodes.Count == 0))
+            //    textWriter.WriteAttributeString(node.Text);
+
+
+
+            foreach (var node in nodesCollection.OfType<TreeNode>())
+            {
+                textWriter.WriteStartElement(node.Text);
+                SaveNodes(node.Nodes, textWriter);
+                textWriter.WriteEndElement();
+            }
+        }
+
+        // Add the children of this XML node 
+        // to this child nodes collection.
+        private void AddTreeViewChildNodes(
+            TreeNodeCollection parent_nodes, XmlNode xml_node)
+        {
+            foreach (XmlNode child_node in xml_node.ChildNodes)
+            {
+                // Make the new TreeView node.
+                TreeNode new_node = parent_nodes.Add(child_node.Name);
+
+                // Recursively make this node's descendants.
+                AddTreeViewChildNodes(new_node.Nodes, child_node);
+
+                // If this is a leaf node, make sure it's visible.
+                if (new_node.Nodes.Count == 0) new_node.EnsureVisible();
+            }
+        }
+
+        // Load a TreeView control from an XML file.
+        private void LoadTreeViewFromXmlFile()
+        {
+            // Load the XML document.
+            XmlDocument xml_doc = new XmlDocument();
+            xml_doc.Load("treestructure.xml");
+
+            // Add the root node's children to the TreeView.
+            treeView1.Nodes.Clear();
+            //treeView1.Nodes.Add(new
+            //    TreeNode(xml_doc.DocumentElement.Name));
+            TreeNode parent = treeView1.Nodes.Add(xml_doc.FirstChild.Name);
+            AddTreeViewChildNodes(parent.Nodes, xml_doc.DocumentElement);
+
+            treeView1.Sort();
+        }
+
+        //private void maketree()
+        //{
+        //    XElement contacts = XElement.Load("treestructure.xml");
+        //    System.Console.WriteLine(contacts.Element("Genders").Elements("Gender").ToList()[0].Value);
+        //    treeView1.BeginUpdate();
+        //    TreeNode defaulet = treeView1.Nodes.Add(contacts.Name.ToString());
+        //    defaulet.Nodes.Add(contacts.Element("Genders").Elements("Gender").ToList());
+        //    treeView1.EndUpdate();
+        //}
+
+            
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+
+            XmlWriter writer = null;
+
+            try
+            {
+
+                // Create an XmlWriterSettings object with the correct options.
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.IndentChars = ("\t");
+                settings.OmitXmlDeclaration = true;
+
+                // Create the XmlWriter object and write some content.
+                writer = XmlWriter.Create("treestructure.xml", settings);
+                //writer.WriteStartElement("Default");
+                SaveNodes(treeView1.Nodes, writer);
+                //writer.WriteEndAttribute();
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+
+            parsedData.TableName = "ADGroups";
+            parsedData.WriteXml("DataXML.xml", XmlWriteMode.WriteSchema);
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTreeViewFromXmlFile();
+            parsedData.ReadXml("DataXML.xml");
+            dataGridView1.DataSource = parsedData;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                column.Visible = false;
+            }
+
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+
+            //selectedNode = treeView1.Nodes["Default"];
+        }
+
+        private void treeView1_AfterLabelEdit(object sender,
+         System.Windows.Forms.NodeLabelEditEventArgs e)
+        {
+            if (e.Label != null && parsedData.Columns[e.Label] == null)
+            {
+                parsedData.Columns.Add(e.Label);
+                dataGridView1.Columns[e.Label].Visible = false;
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode newChild = treeView1.SelectedNode.Nodes.Add("New");
+            treeView1.SelectedNode.Expand();
+            //treeView1.SelectedNode = newChild;
+            newChild.BeginEdit();
+        }
+
+        private void newToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            string clipData = Clipboard.GetText();
+            string[] clipdatas = clipData.Split('\n');
+            if(clipdatas[clipdatas.Length-1] == "")
+            {
+                clipdatas = clipdatas.Take(clipdatas.Count() - 1).ToArray();
+            }
+
+            int cellRow = dataGridView1.SelectedCells[0].RowIndex-1;
+            int cellColumn = dataGridView1.SelectedCells[0].ColumnIndex;
+
+            foreach (string clip in clipdatas)
+            {
+                if (cellRow + 1 >= parsedData.Rows.Count)
+                {
+                    DataRow row = parsedData.NewRow();
+                    parsedData.Rows.Add(row);
+                    //dataGridView1.Rows.Add(row);
+                    //parsedData.AcceptChanges();
+                    //dataGridView1.DataSource = parsedData;
+
+                }
+                cellRow = cellRow + 1;
+
+                parsedData.Rows[cellRow][cellColumn] = clip;
+            }
+
+            parsedData.AcceptChanges();
+
+            //DataGridViewCell cell = dataGridView1.SelectedCells[0];
+            //int index = 0;
+            //foreach(string clip in clipdatas)
+            //{
+            //    cell.Value = clip;
+
+            //    if (cell.RowIndex + 1 >= parsedData.Rows.Count)
+            //    {
+            //        DataRow row = parsedData.NewRow();
+            //        parsedData.Rows.Add(row);
+            //        //dataGridView1.Rows.Add(row);
+            //        //parsedData.AcceptChanges();
+            //        //dataGridView1.DataSource = parsedData;
+                    
+            //    }
+
+            //    cell = dataGridView1.Rows[cell.RowIndex+1].Cells[cell.ColumnIndex];
+            //}
+        }
+    }
+}
